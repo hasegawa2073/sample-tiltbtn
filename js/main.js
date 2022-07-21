2df1797e92018b5042bf711200e5660d8338956e
hasegawa2073 <hasetatsu2001@icloud.com>
2022-07-21 23:26:32 +0900

    Add: ボタンの要素を取得

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+  const btn = document.querySelector('.tiltbtn'); //ボタン自体を取得
+  const btnW = btn.clientWidth; //ボタンの横幅
+  const btnH = btn.clientHeight; //ボタンの高さ
+  btn.addEventListener('mousemove', function (e) {
+    let mouseX = e.offsetX;
+    let mouseY = e.offsetY;
+  });
+});
