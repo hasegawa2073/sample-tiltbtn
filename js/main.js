dbc22d91aac1809d7a0ae187c4d0abc376e710ec
hasegawa2073 <hasetatsu2001@icloud.com>
2022-07-22 18:09:52 +0900

    Fix: ボタンの背景色をつけるのを忘れていたので追加

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const btnW = btn.clientWidth; //ボタンの横幅
   const btnH = btn.clientHeight; //ボタンの高さ
   const transformDeg = 30;
+  const maxShadowSize = 20;
   btn.addEventListener('mousemove', function (e) {
     let mouseX = e.offsetX;
     let mouseY = e.offsetY;
@@ -24,8 +25,44 @@ document.addEventListener('DOMContentLoaded', function () {
     btn.style.transform = `scale3d(1.08, 1.08, 1.08) rotateX(${
       rotate(rateX, rateY, transformDeg)[0]
     }deg) rotateY(${rotate(rateX, rateY, transformDeg)[1]}deg)`;
+
+    const shadow = (x, y, size) => {
+      let result = [maxShadowSize, maxShadowSize]; // 初期値
+      if (x <= 0 && y <= 0) {
+        result[0] = -x * size; // darkのx座標のオフセット
+        result[1] = -y * size; // darkのy座標のオフセット
+        result[2] = 20 - result[0]; // lightのx,y座標値のオフセット
+        return result;
+      }
+      if (x >= 0 && y <= 0) {
+        result[0] = -x * size;
+        result[1] = y * size;
+        result[2] = result[0];
+        return result;
+      }
+      if (x >= 0 && y >= 0) {
+        result[0] = -x * size;
+        result[1] = -y * size;
+        result[2] = 20 + result[0];
+        return result;
+      }
+      if (x <= 0 && y >= 0) {
+        result[0] = -x * size;
+        result[1] = -y * size;
+        result[2] = 20 - result[0];
+        return result;
+      }
+    };
+    // console.log(shadow(rateX, rateY, maxShadowSize)[0]);
+    shadow();
+    btn.style.boxShadow = `${shadow(rateX, rateY, maxShadowSize)[0]}px ${
+      shadow(rateX, rateY, maxShadowSize)[1]
+    }px 20px #cccccc, -${shadow(rateX, rateY, maxShadowSize)[2]}px -${
+      shadow(rateX, rateY, maxShadowSize)[2]
+    }px 20px #ffffff`;
   });
   btn.addEventListener('mouseout', function () {
     btn.style.transform = `scale3d(1.0, 1.0, 1.0) rotateX(0) rotateY(0)`;
+    btn.style.boxShadow = `10px 10px 10px #cccccc, -10px -10px 10px #ffffff`;
   });
 });
