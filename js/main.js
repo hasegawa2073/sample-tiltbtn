dbfbbe6da80c89e82238c039f7a7d32c84f69710
hasegawa2073 <hasetatsu2001@icloud.com>
2022-07-22 15:59:54 +0900

    Add: ボタンが傾く動きの機能追加

--- js/main.js
+++ js/main.js
@@ -2,10 +2,27 @@ document.addEventListener('DOMContentLoaded', function () {
   const btn = document.querySelector('.tiltbtn'); //ボタン自体を取得
   const btnW = btn.clientWidth; //ボタンの横幅
   const btnH = btn.clientHeight; //ボタンの高さ
+  const transformDeg = 30;
   btn.addEventListener('mousemove', function (e) {
     let mouseX = e.offsetX;
     let mouseY = e.offsetY;
-    let rateX = mouseX / btnW;
-    let rateY = mouseY / btnH;
+    let rateX = (mouseX / btnW - 0.5) * 2; //  -1 <= rateX <= 1
+    let rateY = (mouseY / btnH - 0.5) * 2; //  -1 <= rateY <= 1
+    const rotate = (x, y, deg) => {
+      let result = [];
+      if ((x <= 0 && y <= 0) || (x >= 0 && y >= 0)) {
+        result[0] = x * deg;
+        result[1] = -y * deg;
+        return result;
+      }
+      if ((x >= 0 && y <= 0) || (x <= 0 && y >= 0)) {
+        result[0] = -x * deg;
+        result[1] = y * deg;
+        return result;
+      }
+    };
+    btn.style.transform = `scale3d(1.1, 1.1, 1.1) rotateX(${
+      rotate(rateX, rateY, transformDeg)[0]
+    }deg) rotateY(${rotate(rateX, rateY, transformDeg)[1]}deg)`;
   });
 });
