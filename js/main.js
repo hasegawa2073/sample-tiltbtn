be2af0e305afd90a3424e516d3473179f88a0846
hasegawa2073 <hasetatsu2001@icloud.com>
2022-07-22 18:38:12 +0900

    Fix: 影のつき方がおかしかったので修正

--- js/main.js
+++ js/main.js
@@ -28,28 +28,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const shadow = (x, y, size) => {
       let result = [maxShadowSize, maxShadowSize]; // 初期値
-      if (x <= 0 && y <= 0) {
+      if ((x <= 0 && y <= 0) || (x <= 0 && y >= 0)) {
         result[0] = -x * size; // darkのx座標のオフセット
         result[1] = -y * size; // darkのy座標のオフセット
         result[2] = 20 - result[0]; // lightのx,y座標値のオフセット
         return result;
       }
-      if (x >= 0 && y <= 0) {
-        result[0] = -x * size;
-        result[1] = y * size;
-        result[2] = result[0];
-        return result;
-      }
-      if (x >= 0 && y >= 0) {
+      if ((x >= 0 && y <= 0) || (x >= 0 && y >= 0)) {
         result[0] = -x * size;
         result[1] = -y * size;
         result[2] = 20 + result[0];
-        return result;
-      }
-      if (x <= 0 && y >= 0) {
-        result[0] = -x * size;
-        result[1] = -y * size;
-        result[2] = 20 - result[0];
         return result;
       }
     };
