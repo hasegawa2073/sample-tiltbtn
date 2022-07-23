e57b9a6da25f08cb4ee2d4be2079e2b69d3a57ca
hasegawa2073 <hasetatsu2001@icloud.com>
2022-07-23 10:33:40 +0900

    Remove: クラスにする前の不要なコードを削除

--- js/main.js
+++ js/main.js
@@ -70,46 +70,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const tiltbtn = new Tilt(e);
     tiltbtn.rotate();
     tiltbtn.shadow();
-    // let mouseX = e.offsetX; //0 <= mouseX <= width
-    // let mouseY = e.offsetY; //0 <= mouseY <= height
-    // let rateX = (mouseX / btnW - 0.5) * 2; //-1 <= rateX <= 1
-    // let rateY = (mouseY / btnH - 0.5) * 2; //-1 <= rateY <= 1
-    // const rotate = (x, y, deg) => {
-    //   let result = [];
-    //   if ((x <= 0 && y <= 0) || (x >= 0 && y >= 0)) {
-    //     result[0] = x * deg;
-    //     result[1] = -y * deg;
-    //     return result;
-    //   }
-    //   if ((x >= 0 && y <= 0) || (x <= 0 && y >= 0)) {
-    //     result[0] = -x * deg;
-    //     result[1] = y * deg;
-    //     return result;
-    //   }
-    // };
-    // const scale3d = `${transformScale}, ${transformScale}, ${transformScale}`;
-    // const rotateX = rotate(rateX, rateY, transformDeg)[0];
-    // const rotateY = rotate(rateX, rateY, transformDeg)[1];
-    // btn.style.transform = `scale3d(${scale3d}) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-    // const shadow = (x, y, size) => {
-    //   let result = [maxShadowSize, maxShadowSize]; // 初期値
-    //   if ((x <= 0 && y <= 0) || (x <= 0 && y >= 0)) {
-    //     result[0] = -x * size; //darkのx座標のオフセット
-    //     result[1] = -y * size; //darkのy座標のオフセット
-    //     result[2] = size - result[0]; // lightのx,y座標のオフセット
-    //     return result;
-    //   }
-    //   if ((x >= 0 && y <= 0) || (x >= 0 && y >= 0)) {
-    //     result[0] = -x * size;
-    //     result[1] = -y * size;
-    //     result[2] = size + result[0];
-    //     return result;
-    //   }
-    // };
-    // const shadowDarkX = shadow(rateX, rateY, maxShadowSize)[0];
-    // const shadowDarkY = shadow(rateX, rateY, maxShadowSize)[1];
-    // const shadowLightXY = shadow(rateX, rateY, maxShadowSize)[2];
-    // btn.style.boxShadow = `${shadowDarkX}px ${shadowDarkY}px ${shadowBlurSize}px ${darkColor}, -${shadowLightXY}px -${shadowLightXY}px ${shadowBlurSize}px ${lightColor}`;
   });
 
   // タッチしたとき
@@ -118,16 +78,12 @@ document.addEventListener('DOMContentLoaded', function () {
   // 指が動いているとき
   btn.addEventListener('touchmove', function (e) {});
 
-  // ボタンをデフォルトのスタイルに戻す関数
-  const changeBtnDefaultStyle = function () {
-    btn.style.boxShadow = btnDefaultBoxShadow;
-    btn.style.transform = btnDefaultTransform;
-  };
   // マウスが外れたら元に戻す
   btn.addEventListener('mouseout', function (e) {
     const tiltbtn = new Tilt(e);
     tiltbtn.changeBtnDefaultStyle();
   });
+
   // タッチが外れたら元に戻す
   btn.addEventListener('touchend', function (e) {
     const tiltbtn = new Tilt(e);
